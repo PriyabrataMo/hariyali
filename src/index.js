99167d56b3da4ba8777d4d85adc4f7b872c7b45d
Priyabrata <priyabrata8558@gmail.com>
2024-02-14 02:48:47 +0530

    modularised code to conned db

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-import express from "express";
-import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
-mongoose.connect(process.env.MONGO)
-  .then(
-    () => { console.log('MongoDB is connected') }
-  ).catch((err) => {
-    console.log(err);
-  });
 
+import connectDB from "./db/index.js";
+connectDB()
 
 const app = express();
-
-
 app.listen(3000, () => {
   console.log('Server is running on port 3000');  
 }
