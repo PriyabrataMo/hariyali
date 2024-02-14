d7d2f5a4104380a6c6a898fd5b84d1e09ae8a735
Priyabrata <priyabrata8558@gmail.com>
2024-02-14 12:27:16 +0530

    add user model and somthing more

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
+import { app } from './app.js';
 import connectDB from "./db/index.js";
 connectDB()
-
-const app = express();
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');  
-}
-);
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log("Server is running at PORT ",process.env.PORT);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection Falied: ",err);
+})
